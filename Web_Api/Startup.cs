8bb276ee1cbc31e341fff7d1f2b5f3b40a4cc5bf
gluisjulian <gluisjulian@gmail.com>
2021-08-16 14:12:04 -0300

    Migrations

--- Web_Api/Startup.cs
+++ Web_Api/Startup.cs
@@ -1,32 +1,34 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Web_Api.Model.Context;
 using Web_Api.Business;
 using Web_Api.Business.Implementation;
 using Web_Api.Repository;
 using Web_Api.Repository.Implementation;
+using Serilog;
+using System;
+using System.Collections.Generic;
 
 namespace Web_Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
         }
 
-        public IConfiguration Configuration { get; }
+        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -36,8 +38,16 @@ namespace Web_Api
 
             //Injecao de dependencia -> Conexao com banco MYSQL
             var connection = Configuration["MysqlConnection:MysqlConnectionString"];
-            var serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
-            services.AddDbContext<MySqlContext>(options => options.UseMySql(connection, serverVersion));
+            //var serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
+            //services.AddDbContext<MySqlContext>(options => options.UseMySql(connection, serverVersion));
+            services.AddDbContext<MySqlContext>(options => options.UseMySql(connection));
+
+
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
+
 
 
             //API Versioning
@@ -69,5 +79,27 @@ namespace Web_Api
                 endpoints.MapControllers();
             });
         }
+
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+
+                };
+
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Migration Failed", ex);
+                throw;
+            }
+        }
+
     }
 }
